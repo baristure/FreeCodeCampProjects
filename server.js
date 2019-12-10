151e5c585fb60c5c33be6e4206ed8a6a73b2b56a
Glitch (bdev-fccexercisetracker) <none>
2019-12-10 18:18:59 +0000

    ❤️💵 Updated with Glitch

--- server.js
+++ server.js
@@ -1,98 +1,43 @@
-/*
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-
-const cors = require('cors')
-
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
-
-app.use(cors())
-
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
-
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
-
-
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
-
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
-
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
-*/
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
 const cors = require('cors');
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI);
 
 app.use(cors());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-
 app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-// Error Handling middleware
+
+// Middleware error handling
 app.use((err, req, res, next) => {
-    let errCode, errMessage
+    let errCode;
+    let errMessage;
 
     if (err.errors) {
-        // mongoose validation error
+        //mongoose validation error
         errCode = 400 // bad request
-        const keys = Object.keys(err.errors)
-        // report the first validation error
+        const keys = Object.keys(err.errors);
+        //report the first validation error
         errMessage = err.errors[keys[0]].message
     } else {
-        // generic or custom error
+        //generic custom error
         errCode = err.status || 500
         errMessage = err.message || 'Internal Server Error'
     }
-    res.status(errCode).type('txt')
-        .send(errMessage)
+    res.status(errCode).type('txt').send(errMessage)
 });
 
-const listener = app.listen(3000, () => {
-    console.log('Your app is listening on port ' + listener.address().port)
-});
-
+//create userSchema
 let userSchema = new mongoose.Schema({
     username: String
 });
@@ -107,36 +52,34 @@ app.post('/api/exercise/new-user', (req, res) => {
     }, (err, storedUsername) => {
         if (err) return;
         if (storedUsername) {
-            res.send('The username \'' + username + '\' has already been taken.');
+            res.send('the username <' + username + ' > has already been taken :(');
         } else {
             let newUser = new User({
                 username: username
             });
-
             newUser.save((err, createdUser) => {
                 if (err) return;
                 res.json({
                     username: username,
-                    _id: createdUser._id
+                    _id: createdUser.id
                 });
-            });
+            })
         }
-    });
+    })
 });
 
-// Get all users
+//get all users 
 app.get('/api/exercise/users', (req, res) => {
     User.find({}, 'username _id', (err, users) => {
         let output = [];
-
         users.map((user) => {
             output.push(user);
         });
-
         res.send(output);
     });
 });
 
+// create exerciseShema
 let exerciseSchema = new mongoose.Schema({
     userId: {
         type: String,
@@ -158,7 +101,7 @@ let exerciseSchema = new mongoose.Schema({
 
 let Exercise = mongoose.model('Exercise', exerciseSchema);
 
-// Adds a new exercise for a user
+// Add new exercise for a user
 app.post('/api/exercise/add', (req, res) => {
     let userId = req.body.userId;
     let description = req.body.description;
@@ -169,15 +112,13 @@ app.post('/api/exercise/add', (req, res) => {
         if (err) return;
         if (user) {
             let newExercise = new Exercise({
-                userId: user._id,
+                userId: user.Id,
                 description: description,
                 duration: duration
             });
-
             if (date.length > 0) {
                 newExercise.date = new Date(date);
             }
-
             newExercise.save((err, createdExercise) => {
                 if (err) return;
                 res.json({
@@ -188,28 +129,26 @@ app.post('/api/exercise/add', (req, res) => {
                     _id: createdExercise._id
                 });
             });
-        }
-    });
-});
 
-// Get list of all exercises for a given user
+        }
+    })
+})
+
+//get list of all exercises
 app.get('/api/exercise/log/:userId', (req, res) => {
     let userId = req.params.userId;
     let from = req.query.from;
     let to = req.query.to;
     let limit = req.query.limit;
 
-    User.findById(userId, 'username _id', (err, user) => {
+    User.findById(userId, 'username_id', (err, user) => {
         if (err) return;
-
         if (from === undefined) {
             from = new Date(0);
         }
-
         if (to === undefined) {
             to = new Date();
         }
-
         if (limit === undefined) {
             limit = 0;
         } else {
@@ -236,5 +175,9 @@ app.get('/api/exercise/log/:userId', (req, res) => {
             });
         });
     });
+});
 
+//Listen requests:)
+const listener = app.listen(3000, () => {
+    console.log('Your app is listening on port ' + listener.address().port)
 });
