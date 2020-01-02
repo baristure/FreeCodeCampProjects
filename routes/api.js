c31ff06bd8fe3573a581c56a3a3a7de619a27de5
Glitch (tartan-track) <none>
2020-01-02 12:05:54 +0000

    👩🐈 Updated with Glitch

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,10 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
 'use strict';
 
@@ -14,28 +14,207 @@ var ObjectId = require('mongodb').ObjectID;
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app) {
+module.exports = function (app, db) {
 
+  // We want it to be possible to submit (and retrieve, update, delete) issues for various projects. 
+  // To this end, our API route has a parameter (i.e: ":project"):
   app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+
+    /*
+    USER STORY 6:
+    I can GET / api / issues / { project }
+    for an array of all issues on that specific project with all the information
+    for each issue as wasreturned when posted.
+    USER STORY 7:
+    I can filter my get request by also passing along any field and value in the query(ie. / api / issues / { project } ? open = false).
+    I can pass along as many fields / values as I want.
+    */
+    .get(function (req, res) {
       var project = req.params.project;
-      
+      // We'll save the project name, adding "issueTracker_" to its start.
+      // This way, we'll have a specific (and identifiable) collection in the database for each project:
+      let collectionName = "issueTracker_" + req.params.project;
+      //We'll also save all of the query parameters to a variable:
+      let query = req.query;
+      // Because the request content is a string, and the "open" property of our issues should be a boolean,
+      // we need to convert any query for "open" from string to boolean:
+      if (query.open == "true") {
+        query.open = true;
+      } else if (query.open == "false") {
+        query.open = false;
+      }
+      // With these in hand, we can now search our database's project-specific collection for all issues that match the query parameters we've received:
+      // N.B. MongoDB's .find() doesn't take a callback. For that, we tack on .toArray() and have our callback in there.
+      db.collection(collectionName).find(query).toArray((err, data) => {
+        // We'll handle any errors that might arise from this remote/async activity...
+        if (err) return console.log("Error retrieving issues from database:", err);
+
+        // ... and if we don't have any errors, we'll respond with the data:
+        return res.json(data);
+
+      }); //   END of callback and END of .toArray()
     })
-    
-    .post(function (req, res){
-      var project = req.params.project;
-      
+
+
+
+    /*
+      USER STORY 2:I can POST / api / issues / { project } 
+      with form data containing required issue_title, issue_text, created_by, and optional assigned_to and status_text.
+    */
+    /* 
+      USER STORY 3: The object saved(and returned) will include all of those fields(blank
+      for optional no input) and also include created_on(date / time), updated_on(date / time), open(boolean, true
+      for open, false
+      for closed), and _id.
+      */
+
+    .post(function (req, res) {
+      // We'll save the project name, adding "issueTracker_" to the start of it. This way, we'll have a specific collection in the database for each project:
+      let collectionName = "issueTracker_" + req.params.project;
+
+      // We'll also save the information submitted via the form, adding in dates, the open status, and blanks for the optional fields that might not have been filled out:
+      let issue = {
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_on: new Date(),
+        updated_on: new Date(),
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || "",
+        open: true,
+        status_text: req.body.status_text || ""
+      }
+
+      // Next, we'll do some server-side validation to make sure that all the required fields have been submitted:
+      if (typeof issue.issue_title == "undefined" || typeof issue.issue_text == "undefined" || typeof issue.created_by == "undefined") {
+        return res.json("missing required fields");
+      };
+
+      // If all the required fields have been submitted, we'll then add the new issue to a project-specific collection in our database...
+      db.collection(collectionName).insertOne(issue, (err, data) => {
+        // ... making sure to use the callback function to handle any errors that might arise from the remote/async activity...
+        if (err) return console.log("Error POSTing new issue:", err);
+
+        // ... and if there are no errors, we'll return a JSON object containing all of the data for the issue we've just added:
+        return res.json({
+          _id: data.ops[0]._id,
+          issue_title: data.ops[0].issue_title,
+          issue_text: data.ops[0].issue_text,
+          created_on: data.ops[0].created_on,
+          updated_on: data.ops[0].updated_on,
+          created_by: data.ops[0].created_by,
+          assigned_to: data.ops[0].assigned_to,
+          open: data.ops[0].open,
+          status_text: data.ops[0].status_text
+        });
+
+      }) //  END of .insertOne()
+    }) //  END of POST route handler
+
+    /*
+    USER STORY 4:
+    I can PUT / api / issues / { project }
+    with a _id and any fields in the object with a value to object said object.
+    Returned will be‘ successfully updated’ or‘ could not update‘ + _id.
+    This should always update updated_on.If no fields are sent
+    return‘ no updated fieldsent’.
+    */
+    .put(function (req, res) {
+
+      // We'll save the project name, adding "issueTracker_" to its start. This way, we'll have a specific (and identifiable) collection in the database for each project:
+      let collectionName = "issueTracker_" + req.params.project;
+      // We'll also save the information submitted via the form:
+      let issue = req.body;
+
+      // With our data in hand, we'll start by doing some server-side validation to make sure that the required field has been submitted:
+      if (typeof issue._id == "undefined") return res.json("missing required fields");
+
+      // Because the request content is a string, and the "open" property of our issues is a boolean, we need to convert requests to set open=false from string to boolean:
+      if (issue.open == "false") {
+        issue.open = false;
+      };
+
+      // We'll only want to update the fields of our database entry/document/instance for which the user has provided information, so let's discard any empty fields:
+      for (let element in issue) {
+        if (issue[element].length == 0) {
+          delete issue[element];
+        };
+      };
+
+      // If no updated fields are submitted (other than _id), we can simply return "no updated field sent" as per the user stories:
+      if (Object.keys(issue).length == 1) return res.json("no updated field sent");
+
+      // If we DO have some fields to update, before we attempt to update the database entry, we'll add in the updated_on property...
+      issue.updated_on = new Date();
+
+      // Next, for the MongoDB query to work, we need to convert the _id string we currently have into an ObjectId, making sure that the _id entered is valid to avoid errors:
+      if (ObjectId.isValid(issue._id)) {
+        issue._id = ObjectId(issue._id);
+      } else {
+        return res.json("could not update " + issue._id);
+      }
+
+
+      // With our information cleaned up, let's go ahead and attempt to update the given issue in the project-specific collection of our database:
+      db.collection(collectionName).findAndModify({
+          _id: issue._id
+        }, {}, {
+          $set: issue
+        }, // set only the fields that we have in our issue object
+        {
+          new: true
+        }, // returns the updated document/instance rather than the pre-updated version
+        (err, data) => {
+          // We'll handle any errors that might arise from the remote/async action...
+          if (err) return console.log("Error updating issue:", err);
+
+          // ... and if there are no errors and we successfully updated an issue/doc/instance, then we'll respond with messages according to the user stories:          
+          if (data.lastErrorObject.updatedExisting) {
+            return res.json("successfully updated");
+          } else {
+            return res.json("could not update " + issue._id);
+          };
+
+        } // END of callback function
+      ) // END of .findAndModify()
     })
-    
-    .put(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      var project = req.params.project;
-      
-    });
-    
+    /*
+    USER STORY 5:
+    I can DELETE /api/issues/{project} with a _id to completely delete an issue. 
+    If no _id is sent return ‘_id error’, success: ‘deleted +_id, failed: ‘could not delete ‘+_id.
+    */
+    .delete(function (req, res) {
+      // We'll save the project name, adding "issueTracker_" to its start. This way, we'll have a specific (and identifiable) collection in the database for each project:
+      let collectionName = "issueTracker_" + req.params.project;
+      // We'll also save the information submitted via the form:
+      let issue = req.body;
+
+      // Let's start by converting the received _id string into an ObjectId, first making sure that the format is valid:
+      if (ObjectId.isValid(issue._id)) {
+        issue._id = ObjectId(issue._id);
+      } else {
+        return res.json("_id error");
+      };
+
+      // With our converted id in hand, let's attempt to delete the entry in the project-specific collection within our database:
+      db.collection(collectionName).deleteOne({
+          _id: issue._id
+        },
+        (err, data) => {
+          // We'll handle any errors that might arise from the remote/async activity:
+          if (err) return console.log("Error deleting entry:", err);
+
+          // If there are no errors, we'll respond with messages according to the user stories:
+          // It could be that the submitted ID is a valid ObjectId format, but that there is no match for it in the project-specific collection of issues:
+          if (data.deletedCount == 0) {
+            return res.json("could not delete " + issue._id);
+          } else if (data.deletedCount == 1) {
+            return res.json("deleted " + issue._id);
+          } else {
+            return res.json("something completely unexpected has happened while deleting the issue with id: " + issue._id);
+          };
+
+        } // END of callback function
+      ); // END of .deleteOne()
+    }); //  END of DELETE route handler
+
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,24 +1,29 @@
 /*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
+ *
+ *
+ *       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+ *       -----[Keep the tests in the same order!]-----
+ *       (if additional are added, keep them at the very end!)
+ */
 
 var chaiHttp = require('chai-http');
 var chai = require('chai');
 var assert = chai.assert;
 var server = require('../server');
-
+// 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite('Functional Tests', function () {
+
+// we'll need some valid IDs to run tests on the PUT and DELETE requests.
+// To get some of these, when we run our test POST methods, we'll save the returned IDs here globally, so that we can access them later on:
+  let id1;
+  let id2;
   
-    suite('POST /api/issues/{project} => object with issue data', function() {
-      
-      test('Every field filled in', function(done) {
-       chai.request(server)
+  suite('POST /api/issues/{project} => object with issue data', function () {
+
+    test('Every field filled in', function (done) {
+      chai.request(server)
         .post('/api/issues/test')
         .send({
           issue_title: 'Title',
@@ -27,48 +32,143 @@ suite('Functional Tests', function() {
           assigned_to: 'Chai and Mocha',
           status_text: 'In QA'
         })
-        .end(function(err, res){
+        .end(function (err, res) {
           assert.equal(res.status, 200);
-          
-          //fill me in too!
-          
+          assert.isArray(res.body);
+          assert.property(res.body[0], 'issue_title');
+          assert.property(res.body[0], 'issue_text');
+          assert.property(res.body[0], 'created_on');
+          assert.property(res.body[0], 'updated_on');
+          assert.property(res.body[0], 'created_by');
+          assert.property(res.body[0], 'assigned_to');
+          assert.property(res.body[0], 'open');
+          assert.property(res.body[0], 'status_text');
+          assert.property(res.body[0], '_id');
+
+          // We'll save the ID globally :
+          id1 = res.body._id;
+          assert.equal(res.body.issue_title, "Title");
+          assert.equal(res.body.issue_text, "text");
+          assert.equal(res.body.created_by, "Functional Test - Every field filled in");
+          assert.equal(res.body.assigned_to, "Chai and Mocha");
+          assert.equal(res.body.open, true);
+          assert.equal(res.body.status_text, "In QA");
+
           done();
         });
-      });
-      
-      test('Required fields filled in', function(done) {
-        
-      });
-      
-      test('Missing required fields', function(done) {
-        
-      });
-      
     });
-    
-    suite('PUT /api/issues/{project} => text', function() {
-      
-      test('No body', function(done) {
-        
-      });
-      
-      test('One field to update', function(done) {
-        
-      });
-      
-      test('Multiple fields to update', function(done) {
-        
-      });
-      
+
+    test('Required fields filled in', function (done) {
+      chai.request(server)
+        .post('/api/issues/test')
+        .send({
+          issue_title: 'Another test another title',
+          issue_text: 'text',
+          created_by: 'Functional Test - Required fields filled in'
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body);
+          assert.property(res.body[0], 'issue_title');
+          assert.property(res.body[0], 'issue_text');
+          assert.property(res.body[0], 'created_on');
+          assert.property(res.body[0], 'updated_on');
+          assert.property(res.body[0], 'created_by');
+          assert.property(res.body[0], 'assigned_to');
+          assert.property(res.body[0], 'open');
+          assert.property(res.body[0], 'status_text');
+          assert.property(res.body[0], '_id');
+
+          // We'll save the ID globally : 
+          id2 = res.body._id;
+
+          assert.equal(res.body.issue_title, "Another test another title");
+          assert.equal(res.body.issue_text, "text");
+          assert.equal(res.body.created_by, "Functional Test - Required fields filled in");
+          assert.equal(res.body.assigned_to, "");
+          assert.equal(res.body.open, true);
+          assert.equal(res.body.status_text, "");
+          done();
+        });
     });
-    
-    suite('GET /api/issues/{project} => Array of objects with issue data', function() {
-      
-      test('No filter', function(done) {
-        chai.request(server)
+
+    test('Missing required fields', function (done) {
+      chai.request(server)
+        .post('/api/issues/test')
+        .send({
+          issue_title: 'Last test',
+          created_by: 'Functional Test - Missing required fields'
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, "Missing required fields");
+          done();
+        });
+    });
+
+  });
+
+
+
+  // Start PUT test suite
+
+  suite('PUT /api/issues/{project} => text', function () {
+
+    test('No body', function (done) {
+      chai.request(server)
+        .put("/api/issues/test")
+        .send({
+          _id: id1
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, "There is no updated field to sent");
+          done();
+        });
+    });
+
+    test('One field to update', function (done) {
+      chai.request(server)
+        .put("/api/issues/test")
+        .send({
+          _id: id2,
+          issue_title: "Updated title"
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, "Successfully updated");
+          done();
+        });
+    });
+
+    test('Multiple fields to update', function (done) {
+      chai.request(server)
+        .put("/api/issues/test")
+        .send({
+          _id: id2,
+          issue_title: "Updated title",
+          issue_text: "Updated text"
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, "Successfully updated");
+          done();
+        })
+    });
+  });
+
+  // End PUT test suite
+
+
+  // Start GET test suite
+
+  suite('GET /api/issues/{project} => Array of objects with issue data', function () {
+
+    test('No filter', function (done) {
+      chai.request(server)
         .get('/api/issues/test')
         .query({})
-        .end(function(err, res){
+        .end(function (err, res) {
           assert.equal(res.status, 200);
           assert.isArray(res.body);
           assert.property(res.body[0], 'issue_title');
@@ -82,28 +182,86 @@ suite('Functional Tests', function() {
           assert.property(res.body[0], '_id');
           done();
         });
-      });
-      
-      test('One filter', function(done) {
-        
-      });
-      
-      test('Multiple filters (test for multiple fields you know will be in the db for a return)', function(done) {
-        
-      });
-      
-    });
-    
-    suite('DELETE /api/issues/{project} => text', function() {
-      
-      test('No _id', function(done) {
-        
-      });
-      
-      test('Valid _id', function(done) {
-        
-      });
-      
     });
 
-});
+    test('One filter', function (done) {
+      chai.request(server)
+        .get("/api/issues/test")
+        .query({
+          issue_title: "Updated title"
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body);
+          assert.property(res.body[0], 'issue_title');
+          assert.property(res.body[0], 'issue_text');
+          assert.property(res.body[0], 'created_on');
+          assert.property(res.body[0], 'updated_on');
+          assert.property(res.body[0], 'assigned_to');
+          assert.property(res.body[0], 'open');
+          assert.property(res.body[0], 'status_text');
+          assert.property(res.body[0], '_id');
+          assert.equal(res.body[0].issue_title, "Updated title");
+          done();
+        });
+    });
+
+    test('Multiple filters (test for multiple fields you know will be in the db for a return)', function (done) {
+      chai.request(server)
+        .get("/api/issues/test")
+        .query({
+          issue_title: "Updated title",
+          open: true
+        })
+        .end(function (err, res) {
+          assert.isArray(res.body);
+          assert.property(res.body[0], 'issue_title');
+          assert.property(res.body[0], 'issue_text');
+          assert.property(res.body[0], 'created_on');
+          assert.property(res.body[0], 'updated_on');
+          assert.property(res.body[0], 'assigned_to');
+          assert.property(res.body[0], 'open');
+          assert.property(res.body[0], 'status_text');
+          assert.property(res.body[0], '_id');
+          assert.equal(res.body[0].issue_title, "Updated title");
+
+          assert.equal(res.body[0].issue_title, "Updated title");
+          assert.equal(res.body[0].open, true);
+          done();
+        });
+    });
+  });
+
+  // End GET test suite
+
+  // Start DELETE test suite
+
+  suite('DELETE /api/issues/{project} => text', function () {
+
+    test('No _id', function (done) {
+      chai.request(server)
+        .delete("/api/issues/test")
+        .send({}) // <<<<< There is no id 
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, "_id Error!!");
+          done();
+        });
+    });
+
+    test('Valid _id', function (done) {
+      chai.request(server)
+        .delete("/api/issues/test")
+        .send({
+          _id: id1
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, "deleted " + id1);
+          done();
+        })
+    });
+  });
+  // End DELETE test suite
+
+}); //End Functional Tests
