da8924e28590d80cd52dcc484ecd7c2b2edc7ad3
Glitch (bdev-fccurl) <none>
2020-01-15 15:07:24 +0000

    🌴🔖 Updated with Glitch

--- server.js
+++ server.js
@@ -1,162 +1,125 @@
-/*'use strict';
+'use strict';
 
 var express = require('express');
-var mongo = require('mongodb');
+var MongoClient = require("mongodb").MongoClient;
+var ObjectId = require("mongodb").ObjectId;
 var mongoose = require('mongoose');
-
+var shortId = require('shortid');
+var bodyParser = require('body-parser');
+var validUrl = require('valid-url');
+require('dotenv').config();
 var cors = require('cors');
-
 var app = express();
 
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
-//this project needs a db !! *
-// mongoose.connect(process.env.MONGOLAB_URI);
-
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(cors());
+app.use(express.json());
 
-//his project needs to parse POST bodies 
-// you should mount the body-parser here
+const uri = process.env.MONGO_URI;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000 // Timeout after 5s instead of 30s
+});
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
 
 app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-app.listen(port, function () {
-  console.log('Node.js listening ...');
-});
-*/
-"use strict";
-
-var express = require("express");
-var mongo = require("mongodb");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-var dns = require("dns");
-
-var cors = require("cors");
-
-var app = express();
-
-// Basic Configuration
-var port = process.env.PORT || 3000;
-
-mongoose.connect(process.env.MONGO_URI);
-
-var urlMappingSchema = new mongoose.Schema({
+//Create Schema
+const Schema = mongoose.Schema;
+const urlSchema = new Schema({
   original_url: String,
   short_url: String
-});
+})
+const URL = mongoose.model("URL", urlSchema);
 
-var UrlMapping = mongoose.model("UrlMapping", urlMappingSchema);
 
-app.use(cors());
 
-app.use(bodyParser.urlencoded({ entended: false }));
 
-app.use("/public", express.static(process.cwd() + "/public"));
+app.post('/api/shorturl/new', async function (req, res) {
 
-app.get("/", function(req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
-});
+  const url = req.body.url_input
+  const urlCode = shortId.generate()
 
-app.get("/api/hello", function(req, res) {
-  res.json({ greeting: "hello API" });
-});
-
-app.post("/api/shorturl/new", function(req, res) {
-  var url = req.body.url;
-  var dnsLookup = new Promise(function(resolve, reject) {
-    var result = url.replace(/(^\w+:|^)\/\//, "");
-    dns.lookup(result, function(err, addresses, family) {
-      if (err) reject(err);
-      resolve(addresses);
-    });
-  });
-
-  dnsLookup
-    .then(function() {
-      return checkIfExists(url);
+  // check if the url is valid or not
+  if (!validUrl.isWebUri(url)) {
+    res.status(401).json({
+      error: 'invalid URL'
     })
-    .then(function(data) {
-      if (data.status) {
-        return res.json({ original_url: url, short_url: data.short_url });
+  } else {
+    try {
+      // check if its already in the database
+      let findOne = await URL.findOne({
+        original_url: url
+      })
+      if (findOne) {
+        res.json({
+          original_url: findOne.original_url,
+          short_url: findOne.short_url
+        })
       } else {
-        var shortUrl = shorterUrl();
-        var urlMapping = new UrlMapping({
+        // if its not exist yet then create new one and response with the result
+        findOne = new URL({
           original_url: url,
-          short_url: shortUrl
-        });
-        return saveUrlMapping(urlMapping);
+          short_url: urlCode
+        })
+        await findOne.save()
+        res.json({
+          original_url: findOne.original_url,
+          short_url: findOne.short_url
+        })
       }
+    } catch (err) {
+      console.error(err)
+      res.status(500).json('Server erorr...')
+    }
+  }
+})
+
+
+app.get('/api/shorturl/:short_url?', async function (req, res) {
+  try {
+    const urlParams = await URL.findOne({
+      short_url: req.params.short_url
     })
-    .then(function(original_url) {
-      var shortUrl = shorterUrl();
-      return res.json({ original_url: url, short_url: shortUrl });
-    });
-  dnsLookup.catch(function(reason) {
-    return res.json({ error: "invalid URL" });
-  });
-});
+    if (urlParams) {
+      return res.redirect(urlParams.original_url)
+    } else {
+      return res.status(404).json('No URL found')
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(500).json('Server error')
+  }
+})
 
-app.get("/api/shorturl/:shortUrl", function(req, res) {
-  var redirectPromise = redirectToOriginalUrl(req.params.shortUrl);
-  redirectPromise.then(function(original_url) {
-    return res.redirect(original_url);
-  });
-  redirectPromise.catch(function(reason) {
-    return res.json({ error: "invalid URL" });
-  });
-});
+app.listen(port, () => {
+  console.log(`Server is running on port : ${port}`);
+})
 
-function redirectToOriginalUrl(short_url) {
-  return new Promise(function(resolve, reject) {
-    UrlMapping.findOne({ short_url: short_url }, function(err, doc) {
-      if (err || doc === null) return reject(err);
-      else return resolve(doc.original_url);
-    });
-  });
-}
 
-function checkIfExists(original_url) {
-  return new Promise(function(resolve, reject) {
-    UrlMapping.findOne({ original_url: original_url }, function(err, doc) {
-      if (doc === null || err) resolve({ status: false });
-      else resolve({ status: true, short_url: doc.short_url });
-    });
-  });
-}
 
-function saveUrlMapping(mapping) {
-  return new Promise(function(resolve, reject) {
-    mapping.save(function(err, data) {
-      if (err) return reject(err);
-      else return resolve(null, data);
-    });
-  });
-}
 
-function shorterUrl() {
-  var text = "";
-  var possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < 5; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-  return text;
-}
 
-app.listen(port, function() {
-  console.log("Node.js listening ...");
-});
+
+
+
+
+
